57e7e7615ee5d8c3c704972113b0f557d2af3c7e
kn2411 <kn2411@att.com>
2020-12-22 21:30:52 +0530

    FIRST Springboot app on GitHUb

---
+++ src/test/java/learn/spring/boot/web/app/SpringBootFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.spring.boot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
